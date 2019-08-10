abcc27a371921f34fa51a3cb073d30a4b35fe683
dmorkos1@hotmail.com <dmorkos1@hotmail.com>
2019-08-10 19:08:17 +1000

    Add Javascript training udemy

---
+++ script.js
@@ -0,0 +1,8 @@
+// Terneray operator and switch statements
+
+var firstName = 'David';
+var age = 21;
+
+age >= 18 ? console.log(firstName + 'Drinks beer') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
