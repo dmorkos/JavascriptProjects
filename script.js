b8e8f92b7197b410d72578ccbc60be72873e6637
dmorkos1@hotmail.com <dmorkos1@hotmail.com>
2019-08-10 22:13:56 +1000

    Add Javascript training udemy

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 // Terneray operator and switch statements
 
-var firstName = 'David';
-var age = 21;
+// var firstName = 'David';
+// var age = 21;
 
-age >= 18 ? console.log(firstName + 'Drinks beer') : console.log(firstName + ' drinks juice.');
+// age >= 18 ? console.log(firstName + 'Drinks beer') : console.log(firstName + ' drinks juice.');
 
-var drink = age >= 18 ? 'beer' : 'juice';
+// var drink = age >= 18 ? 'beer' : 'juice';
+
+/*********** Variable mutation and tyoe coersion *************************/
+
+// var firstName = 'John'
+// var age = 28
+
+// console.log(firstName + ' ' + age) 
+// // javascript converts data types as it seems neceassary, e.g it converteed age to 
+// // a string, this is what we call type coersion 
+
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+
+// console.log(firstName + ' is a ' + age + ' year old ' + job  +'. Is he married? '
+// + isMarried );
+
+// // variable mutation 
+// age = 'twenty eight';
+// // Javascript can change the variable type according to the data type
+
+// job = 'driver ';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job  +'. Is he married? '
+// + isMarried );
+
+// var lastName = prompt('WHat is his last name?');
+// console.log(firstName + ' ' + lastName)
+
+/***************** Coding challenge 2 udemy  **********************/
+ 
+var AvgteamJohn = (89 + 120 + 103)/3 
+var AvgteamMike = (116 + 94 + 123)/3 
+
+if(AvgteamJohn > AvgteamMike ) { 
+    console.log('Johns team wins avg is ' +  AvgteamJohn)
+} else {
+    console.log('Mikes team wins avg is ' + AvgteamMike )
+}
+
+
+
+
+
+
+
