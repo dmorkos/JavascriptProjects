4868073e4d3c8a9eb484ddb8ba72279b9b4fc1fe
dmorkos1@hotmail.com <dmorkos1@hotmail.com>
2019-08-12 18:29:35 +1000

    nice

--- script.js
+++ script.js
@@ -37,16 +37,92 @@
 
 /***************** Coding challenge 2 udemy  **********************/
  
-var AvgteamJohn = (89 + 120 + 103)/3 
-var AvgteamMike = (116 + 94 + 123)/3 
+// var AvgteamJohn = (89 + 120 + 103)/3 
+// var AvgteamMike = (116 + 94 + 123)/3 
 
-if(AvgteamJohn > AvgteamMike ) { 
-    console.log('Johns team wins avg is ' +  AvgteamJohn)
-} else {
-    console.log('Mikes team wins avg is ' + AvgteamMike )
+// if(AvgteamJohn > AvgteamMike ) { 
+//     console.log('Johns team wins avg is ' +  AvgteamJohn)
+// } else {
+//     console.log('Mikes team wins avg is ' + AvgteamMike )
+// }
+
+/*******************Objects and properties ********************/
+// object order does not matter 
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function() {
+        this.age =  2018 - this.birthYear
+    }
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'Developer';
+john['isMarried'] = true;
+console.log(john)
+
+var jane = new Object();
+jane.name = 'Jane'
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane)
+
+console.log(john.calcAge())
+
+john.calcAge();
+console.log(john)
+
+var David = {
+    fullName: 'David Morkos',
+    mass: 80,
+    height: 1.60,
+    calcBMI: function() {
+        return this.mass/(this.height*this.height)
+    }
 }
 
+var Tony = {
+    fullName: 'Tony Morkos',
+    mass: 100,
+    height: 1.70,
+    calcBMI: function() {
+        return this.mass/(this.height*this.height)
+    }
+}
+David.BMI = David.calcBMI()
+Tony.BMI = Tony.calcBMI();
 
+console.log(David)
+console.log(Tony)
+
+if(David.BMI > Tony.BMI ) {
+    console.log(David.fullName + ' has a higher BMI ')
+} else if(David.BMI < Tony.BMI) {
+    console.log(Tony.fullName + ' has a higher BMI ')
+} else {
+    console.log('They have the same BMI')
+}
+
+/** *****************Coding challeng 5 udemy */
+
+var Bills = {
+    rest1: 124,
+    rest2: 48,
+    rest3: 268,
+    rest4: 180,
+    rest5: 42,
+    calcTip: function() {
+        
+    }
+};
 
 
 
